58a444340bc5baed1e8c2856c06de06c4955d1d6
vikramreddy26 <mr.charan2605@gmail.com>
2025-01-07 11:23:33 +0530

    user credientials

---
+++ demo011.java
@@ -0,0 +1,20 @@
+public class demo011 {
+    package Core_Java;
+    class CircleAreaDemo1 {
+   
+       void Area() {
+           
+           int r = 5;
+           
+           double ca = Math.PI* Math.pow(r, 2);
+           
+           System.out.println("The circle area is : " + ca);
+       }
+   
+   }
+   public class Demo012 {
+       public static void main(String[] args) {
+           CircleAreaDemo1 obj = new CircleAreaDemo1();
+           obj.Area();
+       }
+   }
