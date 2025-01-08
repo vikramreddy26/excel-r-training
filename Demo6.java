f99f6afbf733fa3ccb82fc677f60f279b7716310
vikramreddy26 <mr.charan2605@gmail.com>
2025-01-08 11:47:02 +0530

    username and password for using whileloop

---
+++ Demo6.java
@@ -0,0 +1,10 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String username = "charan";
+        String password = "charan@123";
+        while ((username == "charan") && (password == "charan@123")) {
+            System.out.println("Hello charan welcome to Mallareddy University");
+            break;
+        }
+    }
+}
