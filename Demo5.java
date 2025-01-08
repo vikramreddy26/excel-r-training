8e79588285f7d72cc72d6b4019995fe35bcc0d42
vikramreddy26 <mr.charan2605@gmail.com>
2025-01-08 11:45:22 +0530

    username and password to print a statement

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        String username = "user001";
+        String pwd = "user_001";
+        while ((username == "user001") && (pwd == "user_001")) {
+            System.out.print("Hello user Happy new year");
+            break;
+        }
+
+    }
+}
