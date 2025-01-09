fd21e321ffe84ae2e34d1078fa5544ae1f384f39
vikramreddy26 <mr.charan2605@gmail.com>
2025-01-09 12:35:34 +0530

    increment and decrement

---
+++ demo14.java
@@ -0,0 +1,21 @@
+class UnaryDemo {
+	void UnaryOp() {
+		int a = 10, b = 5;
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(b--);
+		System.out.println(--b);
+		
+		a = 5; 
+		b = 5;
+		int c = a++ * --b * a;
+		System.out.println(c);
+	}
+
+}
+public class Demo14 {
+    public static void main(String[] args) {
+        UnaryDemo obj = new UnaryDemo();
+        obj.UnaryOp();
+	}
+}
