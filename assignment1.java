5172a4b5fff1ddd18cede0fe5f0753e0316148cc
Ch.Sri Charan Reddy <mr.charan2605@gmail.com>
2025-01-25 12:36:30 +0530

    Assignment given by excel if conditions

---
+++ assignment1.java
@@ -0,0 +1,19 @@
+public class assignment1 {
+        public static void main(String[] args) {
+            
+            int num1 = 10; 
+            int num2 = 20; 
+            int num3 = 30;
+            if(num1 > num2){
+            System.out.println("num1 is greater" + num1);
+            }
+            else if(num2 >num3){
+                System.out.println("num2 is greater" + num2);
+    
+            }
+            else{
+            System.out.println("num3 is greater" + num3);
+            }
+    }
+    
+}
