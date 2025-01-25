1934b21277b4d17be33afaecf07b7d4f32954d00
Ch.Sri Charan Reddy <mr.charan2605@gmail.com>
2025-01-25 12:31:28 +0530

    Validating user inputs is prime or not

---
+++ Demo027.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Demo027 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
