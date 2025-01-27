5cd614917b560a5359001dcc3c8b5ed7e8ec82c8
Ch.Sri Charan Reddy <mr.charan2605@gmail.com>
2025-01-27 12:44:44 +0530

    Is palindrome

---
+++ Demo23.java
@@ -0,0 +1,32 @@
+
+package floder;
+
+import java.util.*;
+
+public class Demo23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt(); 
+        Palindrome obj = new Palindrome();
+        System.out.println(obj.isPalindrome(number)); 
+    }
+}
+class Palindrome{
+    int isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        if (originalNumber == reversedNumber) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
