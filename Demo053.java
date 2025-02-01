465780d9cd160e2012109ed618f8ba3c9c357e81
Ch.Sri Charan Reddy <mr.charan2605@gmail.com>
2025-02-01 12:16:51 +0530

    To Check Strong Number Or Not

---
+++ Demo053.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Demo053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (Strong.isStrong(number)) {
+            System.out.println(number + " is a strong number.");
+        } else {
+            System.out.println(number + " is not a strong number.");
+        }
+    }
+}
+class Strong {
+    public static boolean isStrong(int number) {
+        int sum = 0;
+        int temp = number;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == number;
+    }
+
+    private static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
