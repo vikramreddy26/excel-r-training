8c75b7e6adadd150ce15f6a160bdf0fb83c78570
Ch.Sri Charan Reddy <mr.charan2605@gmail.com>
2025-02-08 12:22:27 +0530

    Printing of index  values of an aray

---
+++ Demo066.java
@@ -0,0 +1,12 @@
+package Core_Java;
+
+public class Demo066 {
+ 
+    public static void main(String[] args) {
+        int [] arr ={12,20,30,45,11};
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+           System.out.println("for element: "+arr[i] + " the index is: "+ i);
+        }
+    }
+}
