ba4c70d5ba2014d19fc8e47d063300c0ea2c2cc0
Ch.Sri Charan Reddy <mr.charan2605@gmail.com>
2025-02-08 12:21:15 +0530

    Printing of an array elements

---
+++ Demo065.java
@@ -0,0 +1,11 @@
+package Core_Java;
+
+public class Demo065 {
+    public static void main(String[] args) {
+        int [] arr ={12,20,30,45,11};
+
+       for (int i : arr) {
+          System.out.println(i);
+       }
+    }
+}
