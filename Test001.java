26e18adb59c7b24baf7506ed7997de6d337095f5
Ch.Sri Charan Reddy <mr.charan2605@gmail.com>
2025-02-08 12:35:00 +0530

    Printing 1 to 10 except 5,7

---
+++ Test001.java
@@ -0,0 +1,11 @@
+package Core_Java;
+
+class  Test001{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i != 5 && i != 7) {
+                System.out.println(i);
+            }
+        }
+    }
+}
